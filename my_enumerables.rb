c01cdc59745330b18d3775d8926d58fe1330aecf
kiptubei <kiptubei@gmail.com>
2020-06-16 13:53:38 +0300

    Completed my_each

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,7 +1,9 @@
 module Enumerable
   def my_each
-    for item in self
-      yield(item)
+    return to_enum(:my_each) unless block_given?
+
+    size.times do |item|
+      yield(self[item])
     end
   end
 end
