122bc8e44f505289a97634c4b072b12b54c568ce
rida-elbahtouri <elbhatouri0123@gmail.com>
2020-06-16 14:49:20 +0100

    add the my_each_with_index a,d my_select methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -6,4 +6,24 @@ module Enumerable
       yield(self[item])
     end
   end
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+
+    size.times do |item|
+      yield(self[item], item)
+    end
+  end
+
+  def my_select
+    return to_enum(:my_each) unless block_given?
+
+    size.times do |_item|
+      new_ar = []
+      my_each do |item|
+        new_ar << item if yield(item)
+      end
+      return new_ar
+    end
+  end
 end
